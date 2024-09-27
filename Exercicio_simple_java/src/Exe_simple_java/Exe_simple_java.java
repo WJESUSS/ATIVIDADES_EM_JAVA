df81599b6d6ed6ed953747b04292c41b3f3d1043
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-09-27 20:48:06 -0300

    Atividades simples em java

---
+++ Exercicio_simple_java/src/Exe_simple_java/Exe_simple_java.java
@@ -0,0 +1,18 @@
+package Exe_simple_java;
+
+import java.util.Scanner;
+
+public class Exe_simple_java {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	System.out.println("Digite seu nome :");
+	String nome =sc.nextLine();
+	
+	System.out.println("Meu nome é " +nome);
+	sc.close();
+
+	}
+
+}
---
+++ Exercicio_simple_java/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Exercicio_simple_java {
+}
